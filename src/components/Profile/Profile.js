49978acd6c58e2974fe7d7adb33e186873713158
Timofey Kafanov <tim.kafanov@gmail.com>
2022-05-17 17:45:49 +0400

    feat: change Profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,19 +1,24 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { CurrentUserContext } from '../../contexts/CurrentUserContext';
+
 import './Profile.css';
 
 function Profile(props) {
+  const currentUser = useContext(CurrentUserContext);
+
   return (
     <main className='profile'>
       <div className='profile__container'>
-        <h2 className='profile__title'>Привет, Виталий!</h2>
+        <h2 className='profile__title'>Привет, {currentUser.name}</h2>
         <section className='profile__info'>
           <div className='profile__name'>
             <p className='profile__key'>Имя</p>
-            <p className='profile__value'>Виталий</p>
+            <p className='profile__value'>{currentUser.name}</p>
           </div>
           <div className='profile__email'>
             <p className='profile__key'>E-mail</p>
-            <p className='profile__value'>pochta@yandex.ru</p>
+            <p className='profile__value'>{currentUser.email}</p>
           </div>
         </section>
         <button className='profile__edit' type='button'>Редактировать</button>
